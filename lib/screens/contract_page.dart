90267e4d4765249936004b7382fad53aac26203f
Ben Cheong <cheongmengben@outlook.com>
2022-03-02 14:47:12 +0800

    symbolName

--- lib/screens/contract_page.dart
+++ lib/screens/contract_page.dart
@@ -1,17 +1,41 @@
-// ignore_for_file: avoid_unnecessary_containers, camel_case_types, prefer_const_constructors
 import 'package:flutter/material.dart';
 
 class ContractPage extends StatefulWidget {
-  const ContractPage({Key? key, required this.data}) : super(key: key);
+  const ContractPage({Key? key, required this.data, this.info,}) : super(key: key);
   final data;
+  final info;
 
   @override
-  _ContractPageState createState() => _ContractPageState(this.data);
+  _ContractPageState createState() => _ContractPageState(this.data, this.info);
 }
 
 class _ContractPageState extends State<ContractPage> {
-  _ContractPageState(this.data);
+  _ContractPageState(this.data, this.info);
+  List output = [];
   final data;
+  final info;
+
+  void comparison (){
+    for (int i = 0; i < info.length; i ++){
+      if (data.contract_id == info[i].contract_id){
+        output.add(info[i]);
+      }
+    }
+    print(output);
+  }
+
+    @override
+  void initState() {
+    comparison();
+    // getDetails(ref_id);
+    //super.initState();
+  }
+
+  // @override
+  // void dispose() {
+  //   super.dispose();
+  // }
+
 
   @override
   Widget build(BuildContext context) {
@@ -32,8 +56,9 @@ class _ContractPageState extends State<ContractPage> {
 
             Container(
               color: Colors.amber,
-              child: Text('${data.id}'),
+              child: Text('${output[0].amount}'),
             ),
+            ElevatedButton(onPressed: () {print(info.length);}, child: Text('Click me')),
           ],
         ),
       ),
--- lib/screens/history_page.dart
+++ lib/screens/history_page.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-
+import 'package:drc/screens/contract_details.dart';
 import 'package:drc/screens/contract_page.dart';
 import 'package:flutter/material.dart';
 import 'package:web_socket_channel/io.dart';
@@ -16,6 +16,9 @@ class HistoryScreen extends StatefulWidget {
 class _HistoryScreenState extends State<HistoryScreen> { 
 
   List<transDetails> dataHistory = [];
+  List<symbolDetails> activeSymbol = [];
+  List<transDetails> detailsContract = [];
+  late String symbol_id;
 
   final channel = IOWebSocketChannel.connect(
       Uri.parse('wss://ws.binaryws.com/websockets/v3?app_id=1089')
@@ -29,41 +32,92 @@ class _HistoryScreenState extends State<HistoryScreen> {
     channel.sink.add('{"statement": 1, "description": 1, "limit": 100}');
   }
 
+  void getActiveSymbol(){
+    channel.sink.add('{"active_symbols": "brief","product_type": "basic"}');
+  }
+
+  dynamic getSymbol(data){
+    activeSymbol = [];
+    if(data['msg_type'] == 'active_symbols'){       
+      for(int j = 0; j <= data['active_symbols'].length -1; j ++){
+        symbol_id = data['active_symbols'][j]['symbol'];
+        setState(() {
+            activeSymbol.add(
+              symbolDetails(
+                symbol: symbol_id.toUpperCase(),
+                displayName: data['active_symbols'][j]['display_name']
+                ),
+              );
+            }
+         );
+        }
+    }
+  }
+
   void getAuthorize() {
     channel.stream.listen((event) {
       final data = jsonDecode(event);
-
+      
       dataHistory = [];
       List<dynamic> time = [];
       List<String> currency = [];
       List<String> typeCurrency = [];
-    
+      List<String> displayName = [];
+
+
+      if(data['msg_type'] == 'active_symbols'){
+        getSymbol(data);
+        sendMessageAuthorize();
+      }
+      //getSymbol(data);
+      //sendMessageStatement();
+      
       if (data['msg_type'] == 'authorize') {
          sendMessageStatement();
-         print(data);
+         //print(data);
        }
-      
+            
       if (data['msg_type'] == 'statement') {
          for (int i = 0; i <= data['statement']['transactions'].length -1; i ++) {
           //  print(data['statement']['transactions'][i]['transaction_time']);
-
+          
            time.add(DateTime.fromMillisecondsSinceEpoch(
             data['statement']['transactions'][i]['transaction_time'] * 1000));
           
           String formattedDate = DateFormat('yyyy-MM-dd HH:mm:ss').format(time[i]);
 
-          // currency.add(data['statement']['transactions'][i]['shortcode']);
-
-          // if (currency[i].contains('R_')){
-          //   String a = currency[i].split('_')[1];
-          //   String b = currency[i].split('_')[2];
-          //   String output = a + '_' + b;
-          //   typeCurrency.add(output);
-          // } else{
-          //   String output = currency[i].split('_')[1];
-          //   typeCurrency.add(output);
-          // }
           
+          currency.add(data['statement']['transactions'][i]['shortcode'].toString());
+
+          /*final regex = RegExp (r'(?<=_)[^R_]+(?=_)');
+          String? match = regex.stringMatch(currency[i].toString());
+          typeCurrency.add(match!); */
+
+          if (currency[i].contains('R_')){
+            String a = currency[i].split('_')[1];
+            String b = currency[i].split('_')[2];
+            String output = a + '_' + b;
+            typeCurrency.add(output);
+          } else if (currency[i].contains('null')){
+            typeCurrency.add(currency[i]);
+          }
+          else{
+            String output = currency[i].split('_')[1];
+            typeCurrency.add(output);
+          }
+          //print(typeCurrency.length);
+
+           for(int j = 0; j <= activeSymbol.length -1; j ++){
+              if(typeCurrency[i] == activeSymbol[j].symbol){
+                displayName.add(activeSymbol[j].displayName);
+                break;
+              }
+              if(typeCurrency[i] == 'null'){
+                displayName.add('null');
+                break;
+              }
+            }  
+
           setState(() {
             dataHistory.add(
               transDetails(
@@ -72,23 +126,40 @@ class _HistoryScreenState extends State<HistoryScreen> {
                 id: data['statement']['transactions'][i]['transaction_id'],
                 amount: data['statement']['transactions'][i]['amount'],
                 balance: data['statement']['transactions'][i]['balance_after'],
-                //crypto: typeCurrency[i],
+                contract_id: data['statement']['transactions'][i]['contract_id'],
+                payout: data['statement']['transactions'][i]['payout'],
+                crypto: typeCurrency[i],
+                symbolName: displayName[i],
                 ),
               );
             }
           );
+          
+            
          };
-         print(dataHistory);
+         //print(dataHistory);
        }
     });
-    
   }
+
+  // dynamic getDetails(ref_id) {
+  //   for (int i=0; i <= dataHistory.length -1; i ++){
+  //     if (dataHistory[i].contract_id == ref_id){
+  //       setState(() {
+  //         detailsContract.add(dataHistory[i]);
+  //       });
+  //     }
+  //   }
+  //   return detailsContract;
+  // }
 
   @override
   void initState() {
-    sendMessageAuthorize();
-    sendMessageStatement();
+    getActiveSymbol();
+    //sendMessageAuthorize();
+    //sendMessageStatement();
     getAuthorize();
+    // getDetails(ref_id);
     super.initState();
   }
 
@@ -143,7 +214,7 @@ class _HistoryScreenState extends State<HistoryScreen> {
                     return GestureDetector(
                       onTap: () => {Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (context) => ContractPage(data: dataHistory[index])))
+                        MaterialPageRoute(builder: (context) => ContractDetails(data: dataHistory[index], info: dataHistory)))
                         },
                       child: Container(
                         margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
@@ -175,8 +246,8 @@ class _HistoryScreenState extends State<HistoryScreen> {
                                                 fontSize: 24,
                                                 fontWeight: FontWeight.bold,
                                                 
-                                                color: dataHistory[index].action == 'buy' ? Colors.green : Colors.red)),
-                                        Text('BTCAUSD',
+                                                color: dataHistory[index].action == 'buy' ? Color.fromRGBO(54, 98, 43, 1) : Color.fromRGBO(232, 69, 69,1))),
+                                        Text('${dataHistory[index].symbolName}',
                                             style: TextStyle(
                                                 fontSize: 12,
                                                 fontWeight: FontWeight.bold)),
@@ -196,12 +267,11 @@ class _HistoryScreenState extends State<HistoryScreen> {
                               const SizedBox(
                                 width: 10,
                               ),
-                              Text(
-                                '${dataHistory[index].amount}',
+                              Text( '${dataHistory[index].amount}',
                                 style: TextStyle(
-                                  fontSize: 30,
+                                  fontSize: 27,
                                   fontWeight: FontWeight.bold,
-                                  color: Colors.red,
+                                  color: dataHistory[index].amount >= 0 ?Color.fromRGBO(54, 98, 43, 1) : Color.fromRGBO(232, 69, 69,1),
                                 ),
                               ),
                             ]),
@@ -226,7 +296,10 @@ class transDetails {
   final dynamic id;
   final dynamic amount;
   final dynamic balance;
-  // final dynamic crypto;
+  final dynamic contract_id;
+  final dynamic payout;
+  final dynamic crypto;
+  final dynamic symbolName;
 
   transDetails({
     required this.action,
@@ -234,9 +307,23 @@ class transDetails {
     this.id,
     this.amount,
     this.balance,
-    // this.crypto,
+    this.contract_id,
+    this.payout,
+    this.crypto,
+    this.symbolName,
   });
 
   @override
-  String toString() => '[ $action , $time , $id , $amount, $balance ]';
+  String toString() => '[ $action , $time , $id , $amount, $balance , $contract_id, $payout, $crypto , $symbolName]';
+}
+
+class symbolDetails {
+  final String symbol;
+  final String displayName;
+
+  symbolDetails({
+  required this.symbol,
+  required this.displayName,
+  });
+   String toString() => '[ $symbol, $displayName]';
 }
