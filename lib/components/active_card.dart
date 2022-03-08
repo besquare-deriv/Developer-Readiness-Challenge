eb1c5ea871f82e1137b99cf9698f3f239b79b616
Anantharao1998 <anantharao1998@gmail.com>
2022-03-08 09:12:19 +0800

    Fixed dispose error in active_card.dart

--- lib/components/active_card.dart
+++ lib/components/active_card.dart
@@ -68,11 +68,6 @@ class _activeCardState extends State<activeCard> {
   }
 
   @override
-  void dispose() {
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     try {
       if (result['proposal_open_contract'] != null) {
@@ -136,7 +131,7 @@ class _activeCardState extends State<activeCard> {
                                   ),
                                 ),
                                 Text(
-                                  " ${profit}",
+                                  " $profit",
                                   style: (profit < 0)
                                       ? TextStyle(
                                           color: Colors.red,
@@ -188,7 +183,7 @@ class _activeCardState extends State<activeCard> {
                                   ),
                                 ),
                                 Text(
-                                  " ${time_remaining}",
+                                  " $time_remaining",
                                   style: TextStyle(
                                     color: Colors.black,
                                     fontWeight: FontWeight.bold,
