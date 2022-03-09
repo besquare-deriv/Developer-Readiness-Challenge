fc795814f83a6065ab2dfcd31d643f007e700bc9
Vimalan <vimvimalan623@gmail.com>
2022-03-09 10:08:58 +0800

    Add token fixed

--- lib/screens/token_test.dart
+++ lib/screens/token_test.dart
@@ -5,27 +5,32 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AddNote extends StatefulWidget {
-  AddNote({this.apiToken});
+  AddNote({this.apiToken, this.email});
   String? apiToken;
+  String? email;
+
   @override
-  _AddNoteState createState() => _AddNoteState(apiToken: apiToken);
+  _AddNoteState createState() =>
+      _AddNoteState(apiToken: apiToken, email: email);
 }
 
 class _AddNoteState extends State<AddNote> {
-  _AddNoteState({this.apiToken});
+  _AddNoteState({this.apiToken, this.email});
 
   String? title;
   String? apiToken;
+  String? email;
 
   @override
   Widget build(BuildContext context) {
+    print(email);
     return CupertinoAlertDialog(
       title: Text("Add in token",
           textAlign: TextAlign.center,
           style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold)),
       content: Center(
           child: Column(children: [
-        Text('Enter the BeRad app API token for "johndoe@gmail.com".',
+        Text('Enter the BeRad app API token for "$email".',
             textAlign: TextAlign.center, style: TextStyle(fontSize: 16)),
         SizedBox(height: 8),
         Card(
