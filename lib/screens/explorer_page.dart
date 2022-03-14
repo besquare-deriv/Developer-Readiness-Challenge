b341d51c08ff0ae3db359da45378bd31798e21a0
nurarif <arif.adli0852@gmail.com>
2022-03-14 17:08:20 +0800

    update  component

--- lib/screens/explorer_page.dart
+++ lib/screens/explorer_page.dart
@@ -4,6 +4,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+import '../components/news.dart';
+
 class ExplorePage extends StatefulWidget {
   const ExplorePage({Key? key}) : super(key: key);
 
